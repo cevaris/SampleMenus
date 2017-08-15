22b0e6538d5c945b80fdc17aede6cbe18e372790
Adam Cardenas <cevaris@gmail.com>
2017-08-14 19:51:14 -0600

    initial

---
+++ SampleMenus/SampleMenusPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace SampleMenus
+{
+    public partial class SampleMenusPage : ContentPage
+    {
+        public SampleMenusPage()
+        {
+            InitializeComponent();
+
+            slider.Value = 0;
+            UpdateSliderLabel();
+        }
+
+        public void OnSliderChanged(object sender, ValueChangedEventArgs args)
+        {
+            if (sender == slider)
+            {
+                UpdateSliderLabel();
+            }
+        }
+
+        private void UpdateSliderLabel()
+        {
+            sliderLabel.Text = $"Slider: {(int)slider.Value}";
+        }
+    }
+
+}
